2243ca0373db88d0169f2b43afda0751fb260244
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-04-11 19:15:07 -0300

    ConteudoAula06

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Conteudo_Aula06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
